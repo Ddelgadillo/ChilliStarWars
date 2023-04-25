b5ed5e1f3fce17ee5c99f481cacfeec1dd390586
Diego Delgadillo <ddelgadillo11@gmail.com>
2023-04-25 15:57:23 -0500

    Added blaster pistol and collision with mando (player)

--- Engine/Game.h
+++ Engine/Game.h
@@ -37,8 +37,10 @@ private:
 	/********************************/
 	/*  User Functions              */
 	void DrawMando(float x, float y, float width, float height);
+	void DrawBlaster(float x, float y, float width, float height);
 	void ScreenBoundary(float& x, float& y, float width, float height);
 	void MoveMando(float& x, float& y);
+	bool BoxCollision(float& x1, float& y1, float& x2, float& y2, float width1, float height1, float width2, float height2);
 	/********************************/
 private:
 	MainWindow& wnd;
@@ -49,5 +51,11 @@ private:
 	float mandoPosY;
 	const float mandoWidth;
 	const float mandoHeight;
+	//Blaster
+	float blasterPosX;
+	float blasterPosY;
+	const float blasterWidth;
+	const float blasterHeight;
+	bool isBlasterCollected = false;
 	/********************************/
 };
